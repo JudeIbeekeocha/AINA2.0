89cc33001277d9e5595e3afdfa54e607f65e4936
Jude Ibe Ekeocha <153955257+JudeIbeekeocha@users.noreply.github.com>
2024-10-26 16:44:46 -0400

    Post user question and response to DOM
    Users can see their messages and response dynamically posted and styled

--- js/main-section.js
+++ js/main-section.js
@@ -1,25 +1,46 @@
-// Option 1
-
-// const sendButton = document.querySelector(".send-button");
-// const inputField = document.querySelector(".input-field");
-// let message = [];
-// sendButton.onclick = () => {
-//   message.push(inputField.value);
-//   console.log(inputField.value);
-//   inputField.value = "";
-//   console.log(message);
-// };
-
-// Option 2
-
-
-let message = []; 
+const inputField = document.querySelector(".input-field");
+const sendButton = document.querySelector(".send-button");
+let contentSection = document.querySelector(".content-container");
 
 function getUserMessage() {
-  const inputField = document.querySelector(".input-field");
-  message.push(inputField.value);
-  alert(inputField.value);
+  let message = ""; // I can take this out later and use as a return function
+  message = inputField.value;
   inputField.value = "";
-  console.log(message);
-
+  return message;
 }
+
+// Create a function that adds the message to the DOM with some styles
+function addMessageToDOM(message) {
+  const userContainer = document.createElement("div");
+  const userMessage = document.createElement("p");
+
+  userContainer.classList.add("user-content-container");
+  userMessage.classList.add("user-message");
+  userMessage.textContent = message;
+  userContainer.appendChild(userMessage);
+  contentSection.appendChild(userContainer);
+}
+
+function sendResponseToDOM(message) {
+  const responseContainer = document.createElement("div");
+  const responseMessage = document.createElement("p");
+
+  responseContainer.classList.add("response-content-container");
+  responseMessage.classList.add("response-message");
+
+  responseMessage.textContent =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur provident atque quibusdam quas, saepe inventore ullam qui deleniti.";
+  responseContainer.appendChild(responseMessage);
+  contentSection.appendChild(responseContainer);
+}
+
+inputField.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    addMessageToDOM(getUserMessage());
+    sendResponseToDOM(getUserMessage());
+  }
+});
+sendButton.addEventListener("click", function () {
+  addMessageToDOM(getUserMessage());
+  sendResponseToDOM(getUserMessage());
+});
